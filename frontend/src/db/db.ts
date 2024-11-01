55711526e02cb1fce4c46302ef47e6c34ab94b6c
AlphaXD2003 <alphaxd2023@gmail.com>
2024-11-01 21:17:35 +0530

    small fix in db.ts

--- frontend/src/db/db.ts
+++ frontend/src/db/db.ts
@@ -1,21 +1,5 @@
 import Dexie, { type EntityTable } from "dexie";
 
-interface Chat {
-  id: number;
-  senderId: string;
-  message: string;
-  receiverId: string;
-  timestamp: any;
-}
-interface GroupChat {
-  id: number;
-  senderId: string;
-  message: string;
-  groupname: string;
-  timestamp: any;
-  type: string | "send" | "receive";
-}
-
 interface Conversation {
   id: string;
   name: string;
@@ -36,15 +20,11 @@ interface Message {
 }
 
 const db = new Dexie("ChatDatabase") as Dexie & {
-  chats: EntityTable<Chat, "id">;
-  groupchats: EntityTable<GroupChat, "id">;
   conversations: EntityTable<Conversation, "id">;
   messages: EntityTable<Message, "id">;
 };
 
 db.version(1).stores({
-  chats: "++id ,senderId, message, receiverId, timestamp",
-  groupchats: "++id ,senderId, message, groupname, timestamp, type",
   conversations:
     "id ,name, lastMessage, lastMessageTimestamp, unreadCount, avatar",
   messages: "id ,conversationId, text, sender, timeStamp",
